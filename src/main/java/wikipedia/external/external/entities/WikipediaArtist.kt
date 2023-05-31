e4f55d471fa5896e9b2d6283f919cdc5e522dfcc
valenbarrera <89612680+valenbarrera@users.noreply.github.com>
2023-05-31 14:51:43 -0300

    Se agrego el url del logo de Wikipedia

--- src/main/java/wikipedia/external/external/entities/WikipediaArtist.kt
+++ src/main/java/wikipedia/external/external/entities/WikipediaArtist.kt
@@ -1,5 +1,6 @@
 package wikipedia.external.external.entities
 
+const val WIKIPEDIA_LOGO = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Wikipedia-logo-v2-es.png"
 data class WikipediaArtist(
     val name : String,
     var artistInfo : String,
